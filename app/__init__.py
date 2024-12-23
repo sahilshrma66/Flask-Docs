5602ae5577e2dddd507731548f22598d6bc5c853
sahilsharmas <sahilsharmas@qasource.com>
2024-12-23 17:25:43 +0530

    updated flask directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,7 @@
-from flask import Flask, send_from_directory
-from flask_swagger_ui import get_swaggerui_blueprint
-import os
+from flask import Flask
 
 
 def create_app():
+    """A function to create the flask app"""
     app = Flask(__name__)
-
-    SWAGGER_URL = '/swagger'
-    API_URL = '/swagger/swagger.yaml'
-
-    # Setup Swagger UI
-    swagger_ui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL,
-        API_URL,
-        config={
-            'app_name': "Flask Solid Login API"
-        }
-    )
-    app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
-
-    @app.route('/swagger/swagger.yaml')
-    def swagger_yaml():
-        return send_from_directory(os.path.join(app.root_path, 'swagger'), 'swagger.yaml')
-
     return app
--- app/routes/auth_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from app.controller.auth_controller import AuthController
-
-auth_bp = Blueprint('auth', __name__)
-
-auth_bp.route('/register', methods=['POST'])(AuthController.register)
-auth_bp.route('/login', methods=['POST'])(AuthController.login)
